497e5d9467aba9df16d8f430790eef8b436903b6
jin hye <sjrry112@gmail.com>
2023-01-20 14:41:29 +0900

    230120_1st_Commit

---
+++ vue-test0120/src/components/HomeVue.vue
@@ -0,0 +1,27 @@
+<script>
+export default{
+  data(){
+    return{
+      numberClass:"number",
+      count: 0
+    }
+  },
+  methods : {
+    increment(){
+      this.count++;
+    }
+  }
+}
+</script>
+
+<template>
+  <h1 v-bind:class="numberClass">{{ count }}</h1>
+  <h3>vue로 확인하는 웹페이지 입니다. 아래 버튼을 누르면 값이 1씩 증가합니다</h3>
+  <button v-on:click="increment" >1+</button>
+</template>
+
+<style>
+.number{
+  color: red;
+}
+</style>
